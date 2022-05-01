e6de520a84719c6ec18135d311920d23d42498e9
GHNuma <mr.ghnuma@gmail.com>
2022-05-01 09:38:17 +0600

    finished filter and search, also added pagination and decoration for no poster films

--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,111 @@
 import './App.css';
-import { useState, useEffect } from 'react';
+import { useState, useEffect,startTransition } from 'react';
 // import Lesson1 from "./components/Lesson_1/Lesson1.jsx"
 import axios from "axios"
 import Card from './components/Card/Card';
-import { Switch, FormGroup, FormControlLabel,FormControl } from '@mui/material';
+import { Switch, FormGroup, FormControlLabel,FormControl, Input, Pagination, Skeleton } from '@mui/material';
 function App() {
+  
+  const Base_URL="https://api.themoviedb.org/3/search/movie?"
+  const api_key="d65708ab6862fb68c7b1f70252b5d91c"
 
+  const [loading,setLoading]=useState(false)
   const [filmsData, setFilmsData]=useState([])
+  const [page,setPage]=useState(1)
   const [filterSettings,setFilterSettings]=useState({
     adult:false,
-    video:false
+    video:false,
+    searching:'spider'
   })
 
   useEffect(()=>{
       const fetchData = async ()=>{
-      await axios.get("https://api.themoviedb.org/3/search/movie?api_key=d65708ab6862fb68c7b1f70252b5d91c&language=ru-RU&page=1&include_adult=false&query=spider")
-      .then(res=>setFilmsData(res.data))
+      setLoading(true)
+      await axios.get(Base_URL+ `api_key=${api_key}&language=ru-RU&page=${page}&include_adult=${filterSettings.adult}&query=${filterSettings.searching}`)
+      .then(res=>{
+        setFilmsData(res.data)
+        setLoading(false)
+      })
     }
     fetchData()
-  },[])
+  },[Base_URL,api_key,filterSettings.adult,filterSettings.searching,page])
+
 
 
   const checkboxHandler=(event)=>{
     setFilterSettings({...filterSettings, [event.target.name]:event.target.checked})
   }
-
+  const handleSearch=(searchEvent)=>{
+    startTransition(setFilterSettings({...filterSettings, [searchEvent.target.name]:searchEvent.target.value}))
+  }
+  const pageChange=(changePageEvent, value)=>{
+    setPage(value)
+  }
+  
   return (
     <div>
+      {/*Пока что такой отступ чтобы контент был виден*/}
+      <br/>
         <div className='filter_container'>
-          <FormControl >
-            <FormGroup aria-label="position" row>
+          <div className='filter_content'>
+            <FormControl>
+              <FormGroup aria-label="position" row>
+                <FormControlLabel
+                control={<Switch color="primary" checked={filterSettings.adult} name="adult" onChange={checkboxHandler} size="medium"/>}
+                label="Adult"
+                labelPlacement="bottom"
+              />
+              </FormGroup>
+            </FormControl>
+            <FormControl >
+              <FormGroup aria-label="position" row>
+                <FormControlLabel
+                control={<Switch 
+                  color="primary" 
+                  checked={filterSettings.video} 
+                  name="video" 
+                  onChange={checkboxHandler} 
+                  size="medium"/>}
+                label="With video"
+                labelPlacement="bottom"
+              />
+              </FormGroup>
+            </FormControl>
+            <FormControl>
               <FormControlLabel
-              control={<Switch color="primary" checked={filterSettings.adult} name="adult" onChange={(e)=>checkboxHandler(e)}/>}
-              label="Adult"
-              labelPlacement="bottom"
-            />
-            </FormGroup>
-          </FormControl>
-          <FormControl >
-            <FormGroup aria-label="position" row>
-              <FormControlLabel
-              control={<Switch color="primary" checked={filterSettings.video} name="video" onChange={(e)=>checkboxHandler(e)}/>}
-              label="With video"
-              labelPlacement="bottom"
-            />
-            </FormGroup>
-          </FormControl>
-
-          <section>
-            <input placeholder='search film by name'/>
-          </section>
+                control={<Input 
+                  placeholder='search film by name'
+                  value={filterSettings.searching}
+                  type="text"
+                  name="searching"
+                  onChange={(e)=>{handleSearch(e)}}
+                  size="medium"
+                  />}
+                label="Search"
+                labelPlacement='bottom'
+              />
+              
+            </FormControl>
+          </div>
         </div>
-         <div className='main_content'>
+        <div className='main_content'>
               {/* <Lesson1/> */}
-          {filmsData.length!==0 ? filmsData.results.map((film)=>{
-              return(
-                <Card info={film}/>
-              )
-            })
-            :
+          {filmsData.length!==0 ? 
+          filmsData.results.map((film)=>(
+            loading ? 
+              <Skeleton variant="rectangular">
+                <Card info={film} key={film.id}/>
+              </Skeleton>
+              : 
+              <Card info={film} key={film.id}/> 
+            ))
+          :
             <div>
             </div>
           }
-
-
-          </div>
+        </div>
+        <Pagination count={filmsData.total_pages} page={page} onChange={pageChange} className="pagination"/>
+        <br/>
     </div>
     
   );
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,8 +3,16 @@ import "./card.css"
 import { Rating } from '@mui/material'
 export default function Card({info}) {
   return (
-    <div className='card_container' style={{backgroundImage:`url(https://image.tmdb.org/t/p/original/${info.poster_path})`}}>
-        <div>
+    <div 
+      className='card_container' 
+      style={
+      info.poster_path ? 
+        {backgroundImage:`url(https://image.tmdb.org/t/p/original/${info.poster_path})`}
+        : 
+        {backgroundImage:"url(empty_poster.png)",backgroundSize:"contain"}
+      }
+    >
+        <div className='card_details'>
             <Rating
             name="simple-controlled"
             className='rating_stars'
@@ -12,9 +20,8 @@ export default function Card({info}) {
             size='small'
             readOnly
             value={info.vote_average/2}
-            // Тут вопрос которое поле показывает рейтинг
             />
-            <p className='film_title'>{info.original_title}</p>
+            <p className='film_title' style={info.poster_path===null ? {color:"black",textDecoration:"line-through"} : {color:"white"}}>{info.original_title}</p>
         </div>
     </div>
   )
